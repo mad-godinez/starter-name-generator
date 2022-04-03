b804384abc0888a83f030d1e548c364ac8fa3cc0
Mad Godinez <madgodinez@gmail.com>
2022-04-03 14:04:15 -0500

    fixed fetch url

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ const App = () => {
   const [name, setName] = useState([])
 
   useEffect(() => {
-    fetch('https://git.heroku.com/backend-generate-name.git')
+    fetch('https://backend-generate-name.herokuapp.com/generate')
       .then((res) => res.json())
       .then((nme) => setName([nme]))
   }, [])
