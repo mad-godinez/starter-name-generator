c2eaad72af32205b0c83482a26a50860442a4a32
Mad Godinez <madgodinez@gmail.com>
2022-04-03 13:59:53 -0500

    changed fetch url to deployed server url

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ const App = () => {
   const [name, setName] = useState([])
 
   useEffect(() => {
-    fetch('https://enigmatic-shore-92552.herokuapp.com/generate')
+    fetch('https://git.heroku.com/backend-generate-name.git')
       .then((res) => res.json())
       .then((nme) => setName([nme]))
   }, [])
